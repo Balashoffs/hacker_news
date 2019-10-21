0e58821cbbac39a2c132759ce2fd61471063ce45
alexbeard <balashov87aleksandr@gmail.com>
2019-10-20 20:20:06 -0700

    22. On Demand Widget Rendering - 10. Bloc Testing

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hacker_news/src/blocs/stories_provider.dart';
-import '../blocs/stories_bloc.dart';
+import '../blocs/stories_provider.dart';
 
 class NewsList extends StatelessWidget {
   @override
@@ -10,7 +9,25 @@ class NewsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Top news!'),
       ),
-      body:  Text(''),
+      body:  buildList(bloc),
+    );
+  }
+
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (context, snapshot){
+        if(!snapshot.hasData){
+          return Text('Still wating on Ids');
+        }
+        return ListView.builder(
+          itemCount: snapshot.data.lenght,
+          itemBuilder: (context, int index){
+            return Text(snapshot.data[index]);
+          },
+
+        );
+      },
     );
   }
 }

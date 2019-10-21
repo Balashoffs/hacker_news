d48680a66d1bd328fc0b818a59de087d8998f5f6
alexbeard <balashov87aleksandr@gmail.com>
2019-10-20 20:01:58 -0700

    22. On Demand Widget Rendering - 8. Exposing Bloc Getters

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -4,8 +4,14 @@ import '../resources/repository.dart';
 
 class StoriesBloc{
   final _topIds = PublishSubject<List<int>>();
+  final _repository = Repository();
 
   Observable<List<int>> get topIds => _topIds.stream;
+
+  fetchIds() async {
+    final ids = await _repository.fetchTopIds();
+    _topIds.sink.add(ids);
+  }
 
   dispose(){
     _topIds.close();

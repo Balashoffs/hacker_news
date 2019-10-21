129266b0e0a8813018d051542ff85917882e4e84
alexbeard <balashov87aleksandr@gmail.com>
2019-10-20 20:07:49 -0700

    22. On Demand Widget Rendering - 9. Wiring Up The Stories Provider

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:hacker_news/src/blocs/stories_provider.dart';
+import '../blocs/stories_bloc.dart';
 
 class NewsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = StoriesProvider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Top news!'),
       ),
-      body: Text('Show list'),
+      body:  Text(''),
     );
   }
 }

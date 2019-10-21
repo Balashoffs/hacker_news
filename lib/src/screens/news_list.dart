8aec4321b218c447b6e45ba16820046d11748419
alexbeard <balashov87aleksandr@gmail.com>
2019-10-20 19:55:53 -0700

    22. On Demand Widget Rendering - 7. Bloc Design

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -7,33 +7,7 @@ class NewsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Top news!'),
       ),
-      body: buildList(),
-    );
-  }
-
-  Widget buildList() {
-    return ListView.builder(
-      itemCount: 1000,
-      itemBuilder: (contex, int index){
-        return FutureBuilder(
-          future: getFuture(),
-          builder: (contex, snapshot){
-            return Container(
-              height: 80,
-              child: snapshot.hasData
-                ? Text('Get $index')
-                : Text("Does not get $index"),
-            );
-          },
-        );
-      },
-    );
-  }
-
-  getFuture() {
-    return Future.delayed(
-      Duration(seconds: 2),
-      ()=> 'HI',
+      body: Text('Show list'),
     );
   }
 }
